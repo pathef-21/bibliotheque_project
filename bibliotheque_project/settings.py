a05962a90b421aa385ac2b60ba3ccf973f4571ff
pathe-21 <pathef566@gmail.com>
2026-04-16 09:07:05 +0000

    modif

--- bibliotheque_project/settings.py
+++ bibliotheque_project/settings.py
@@ -53,13 +53,23 @@ TEMPLATES = [{
 
 WSGI_APPLICATION = 'bibliotheque_project.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+# ─── Base de données ──────────────────────────────────────────────────────────
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    # Production → PostgreSQL Render
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    # Local → SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
